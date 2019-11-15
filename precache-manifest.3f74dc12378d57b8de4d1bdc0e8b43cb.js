cecb29f50b76264da0bc8fc837d1c794a8fd0402
Luthfulahi <luteeph@gmail.com>
2019-11-15 09:06:42 +0100

    Updates

---
+++ precache-manifest.3f74dc12378d57b8de4d1bdc0e8b43cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232a5c81b69591fcbd1d7dd572bef606",
+    "url": "/business-listing/index.html"
+  },
+  {
+    "revision": "5be17f5b31edd14a2c57",
+    "url": "/business-listing/static/css/main.afce2eaf.chunk.css"
+  },
+  {
+    "revision": "ffcc6263ba88e5b9297b",
+    "url": "/business-listing/static/js/2.16c551c3.chunk.js"
+  },
+  {
+    "revision": "5be17f5b31edd14a2c57",
+    "url": "/business-listing/static/js/main.43e72608.chunk.js"
+  },
+  {
+    "revision": "f0b9f20ff5975ce7a6b3",
+    "url": "/business-listing/static/js/runtime-main.2357b6c8.js"
+  }
+]);
